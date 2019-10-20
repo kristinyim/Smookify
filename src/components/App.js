ef1c9b84af46a813780675a4dad90399d59152be
Kristin Yim <k.yim33@gmail.com>
2019-10-20 17:49:52 -0400

    spotify auth, no secret

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../img/logo.svg';
 import '../css/App.css';
+import axios from "axios";
+import {
+  spotifyWebApiURL,
+  spotifyProfileURL
+} from "../helper/constants";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authToken: "",
+      authorized: false,
+      profile: []
+    }
+  }
+
+  componentDidMount = () => {
+    let url = window.location.href;
+    if (url.indexOf("token=") > -1) {
+      let authToken = url
+        .split("token=")[1]
+        .split("&")[0]
+        .trim();
+      let authorized = true;
+      this.setState({ authToken, authorized });
+    }
+  };
+
+  handleAuthFlow = event => {
+    event.preventDefault();
+
+    if (this.state.authorized) {
+      const { authToken } = this.state;
+      let user;
+      axios
+        .get(spotifyProfileURL + authToken)
+        .then(response => {
+          this.setState({ profile: response.data });
+          user = response.data;
+        })
+        .then(() => this.props.history.push('/home', {
+            current_user: { user },
+            auth: { authToken }
+          }))
+        .catch(error => {
+          console.log(error);
+          window.location.assign(spotifyWebApiURL);
+        });
+    } else {
+      window.location.assign(spotifyWebApiURL);
+    }
+  }
+
+  render() {
+    return (
+      <div>
         <p>
-          hello
+          {this.state.authorized
+            ? "Authorized. Enter below"
+            : "Click below to authorize our Spotify account"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        <button
+          type="button"
+          onClick={this.handleAuthFlow}>
+          {this.state.authorized
+            ? "Proceed to Smookify"
+            : "Sign in with Spotify"}
+        </button>
+      </div>
+    );
+  }
+};
---
+++ src/helper/constants.js
@@ -0,0 +1,13 @@
+import {
+  clientID,
+  clientSecret
+} from "./secrets";
+
+let prod = false;
+
+const scopes = "user-read-private+user-read-email+playlist-read-private+user-top-read+user-read-recently-played";
+const redirectURI = prod ? "http://kristinstrawberryyim.com/smookify" : "http://localhost:3000/";
+
+export const spotifyWebApiURL = `https://accounts.spotify.com/authorize/?client_id=${clientID
+}&response_type=token&redirect_uri=${redirectURI}&scope=${scopes}`;
+export const spotifyProfileURL = "https://api.spotify.com/v1/me?access_token=";
